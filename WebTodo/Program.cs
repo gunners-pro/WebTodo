287eb3c3778f4b3dd63daaa6b0fc8cf5e9e62b29
Fabricyo Barreto <bass.barreto@gmail.com>
2024-09-10 16:42:21 -0300

    initial config

---
+++ WebTodo/Program.cs
@@ -0,0 +1,29 @@
+using Radzen;
+using WebTodo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+{
+    builder.Services.AddRadzenComponents();
+}
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
